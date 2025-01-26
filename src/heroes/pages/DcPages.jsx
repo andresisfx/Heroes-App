b21969e6e96107ba9a86535385c6cb93c132f2ff
andresisfx <andresrodriguezwg@gmail.com>
2025-01-25 21:56:24 -0500

    Heroe list created

--- src/heroes/pages/DcPages.jsx
+++ src/heroes/pages/DcPages.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { HeroList } from '../components/HeroList'
 
 export const DcPages = () => {
   return (
     <>
       <h1>DcPages</h1> 
+      <HeroList publisher="DC Comics" />
     </>
   )
 }
--- src/heroes/pages/MarvelPages.jsx
+++ src/heroes/pages/MarvelPages.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import { HeroList } from '../components'
 export const MarvelPages = () => {
   return (
     <>
-      <h1>MarvelPages</h1> 
+      <h1>DcPages</h1> 
+           <HeroList publisher="Marvel Comics" />
     </>
   )
 }
