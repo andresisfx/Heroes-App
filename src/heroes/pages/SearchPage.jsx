b5035b4960d8fd47aa2da9308d4c0dd3b5344432
andresisfx <andresrodriguezwg@gmail.com>
2025-01-28 18:30:31 -0500

    seach ´page created

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import { HeroCard } from '../components'
 
 export const SearchPage = () => {
   return (
     <>
-    <h1>SeacrhPage</h1>
-   </>
+      <h1>Seacrh</h1>
+      <hr />
+      <div className='row'>
+            <div className='col-5'>
+
+                <form action="">
+                <input
+                    type="text"
+                    className='form-control'
+                    placeholder='Find your hero'
+                    aria-label="Find your hero"
+                    name='searchText'
+                    autoComplete='off'
+                />
+                <button
+                    type='submit'
+                    className='btn btn-outline-primary mt-1'
+
+                >Search</button>
+                </form>
+
+            </div>
+            <div className='col-7'>
+                <h4>Results</h4>
+                <hr />
+                <div className='alert alert-primary animate__animated animate__fadeIn'>
+                    Search Hero
+                </div>
+                <div className='alert alert-danger animate__animated animate__fadeIn'>
+                    Not found
+                </div>
+                <HeroCard />
+
+
+            </div>
+      </div>
+    </>
   )
 }
