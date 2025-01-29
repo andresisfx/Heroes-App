25f5c0b8889e23742cda8c9b06eaf4471f68764f
andresisfx <andresrodriguezwg@gmail.com>
2025-01-28 20:55:43 -0500

    project finished

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -24,7 +24,12 @@ export const SearchPage = () => {
         naviagte(`?q=${searchText.toLowerCase().trim()}`);
     }
 
-    const heroes=useMemo(()=>getHeroesByName(q),[q]);
+    const heroes=getHeroesByName(q);
+
+    const showSearch=(q.length===0);
+    const showError=(q.length>0 && heroes.length===0);
+    console.log("error",showError);
+    console.log("search",showSearch);
 
   return (
     <>
@@ -56,14 +61,14 @@ export const SearchPage = () => {
             <div className='col-7'>
                 <h4>Results</h4>
                 <hr />
-                <div className='alert alert-primary animate__animated animate__fadeIn'>
+                <div className='alert alert-primary animate__animated animate__fadeIn' style={{display: showSearch ? '' : 'none'}}>
                     Search Hero
                 </div>
-                {
-                heroes.length < 1 && <div className='alert alert-danger animate__animated animate__fadeIn'>
+
+                <div className='alert alert-danger animate__animated animate__fadeIn'style={{display: showError ? '' : 'none' }}>
                     No hero with <b>{q}</b>
                 </div>
-                }
+                
 
                 {
                   heroes?.map(hero=>(
